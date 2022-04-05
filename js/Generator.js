21d91d8c3a1b0df75c7719992c7e210afffeaef8
Toughee <Jimernez.io@gmail.com>
2022-04-04 21:44:23 -0500

    JsPw/003/Command_update_01
    - Added random password generator script to app
    - Updated UI design for app
    - Updated npm packages

--- js/Generator.js
+++ js/Generator.js
@@ -1,6 +1,19 @@
-// const pwElem = "!@#$%^&)__)(*&+1234567890";
+// Function to generate randomizes word content
+function PwGen() {
 
-// function Generator() {
+    // Password content
+    let pwElem = "!@#$%^&)__)(*&+1234567890-ABCDEFGHIJKLMNOPQRSTUVWXYZ*abcdefghijklmnopqrst";
+    let newPw = '';
 
-//     return newPw;
+
+    for (var i = 0; i < 12; i++)
+        newPw += pwElem.charAt(Math.floor(Math.random() * pwElem.length));
+    
+    // Button selector for password generator
+    document.getElementById("passwordSection").innerHTML = newPw;
+}
+
+// function copyPw() {
+//     document.getElementById("passwordSection");
+// 
 // }
